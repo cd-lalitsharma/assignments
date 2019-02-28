ea2290bc0b237bfd9a7f6e1be9b156e1c24492f5
cd-lalitsharma <lalit.sharma@tothenew.com>
2019-02-28 17:16:49 +0530

    added new assignment java 2

--- introdution_to_java_2/Question_10.java
+++ introdution_to_java_2/Question_10.java
@@ -1,2 +1,37 @@
+
+class customer{
+    
+    protected void placeOrder(){
+    
+    }
+    
+    protected  void orderComplete(){
+    }
+    protected  void pleaseWait(){
+    }
+    private  void enjoyCoffee(){
+    }
+    
+    
+    
+}
+class cashier{
+    protected  void collectOrder(){
+    }
+    
+    protected  void intimatecustomerForCompletedOrder(){
+    
+    }
+    
+    
+    
+}
+class barista{
+    protected  void prepareCoffee(){
+    
+    }
+    
+}
+
 public class Question_10 {
 }
