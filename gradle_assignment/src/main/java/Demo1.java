c7eaf6a3a0042518f6b342ca5968ac7ca28e23bb
cd-lalitsharma <lalit.sharma@tothenew.com>
2019-03-07 15:08:53 +0530

    added gradle assignment

---
+++ gradle_assignment/src/main/java/Demo1.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+//        System.out.println("output from src/main/java");
+//        System.out.println("reversed hello using StringUtils is "+ StringUtils.reverse("hello"));
+    }
+    
+}
